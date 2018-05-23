f53a81065017199e76f83c1769f9677a6f06391b
Mohammad MDSA <samimd.77@gmail.com>
2018-05-23 09:18:00 +0430

    Added checklist item model

---
+++ CurrencyTracker/Models/CheckListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyTracker.Models
+{
+	public class CheckListItem
+	{
+		public Check Check { get; set; }
+		public decimal Total { get; set; }
+	}
+}
